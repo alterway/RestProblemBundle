fea38762634664d6e64ae56cdf67c30c0a72f30f
Kim Laï Trinh <kimlai@lrqdo.fr>
2013-10-30 13:54:42 +0100

    modified unit test

--- test/unit/Problem/InvalidQueryFormTest.php
+++ test/unit/Problem/InvalidQueryFormTest.php
@@ -14,21 +14,39 @@ class InvalidQueryFormTest extends PHPUnit_Framework_TestCase
         // @see https://github.com/sebastianbergmann/phpunit-mock-objects/issues/103
         // $form = $this->getMock('\Symfony\Component\Form\FormInterface', array('all', 'getErrors'));
         $form = $this->getMock('\Symfony\Component\Form\Form', array('all', 'getErrors'), array(), '', false);
-        
+        $child = $this->getMock('\Symfony\Component\Form\Form', array('all', 'getErrors'), array(), '', false);
+        $error1 = new \Symfony\Component\Form\FormError("an error occured in the root form");
+        $error2 = new \Symfony\Component\Form\FormError("an error occured in a child form");
         
         $form
                 ->expects($this->once())
                 ->method('getErrors')
-                ->will($this->returnValue(array('field1' => array('an error occured'))))
+                ->will($this->returnValue(array($error1)))
         ;
         $form
+                ->expects($this->once())
+                ->method('all')
+                ->will($this->returnValue(array('child' => $child)))
+        ;
+
+        $child
+                ->expects($this->once())
+                ->method('getErrors')
+                ->will($this->returnValue(array($error2)))
+        ;
+        $child
                 ->expects($this->once())
                 ->method('all')
                 ->will($this->returnValue(array()))
         ;
 
         $object = new \Alterway\Bundle\RestProblemBundle\Problem\InvalidQueryForm($form);
-        $expected = array('field1' => array('an error occured'));
+        $expected = array(
+            'errors' => array(
+                'an error occured in the root form',
+                'child' => array('an error occured in a child form'),
+            ),
+        );
         $this->assertEquals($expected, $object->getDetail());
     }
     
@@ -54,7 +72,7 @@ class InvalidQueryFormTest extends PHPUnit_Framework_TestCase
         ;
 
         $object = new \Alterway\Bundle\RestProblemBundle\Problem\InvalidQueryForm($form);
-        $expected = array();
+        $expected = array('errors' => array());
         $this->assertEquals($expected, $object->getDetail());
     }
 
